ade82a970b1170cf4be23e1b1188b2f66b33f03f
SoundaryaJagan <38034280+SoundaryaJagan@users.noreply.github.com>
2018-06-29 12:36:45 +0000

    Create rotat.java

---
+++ rotat.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class rotat
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+ArrayList<Integer>a=new ArrayList<Integer>();
+for(int i=0;i<n;i++)
+{
+int c=s.nextInt();
+a.add(c);
+}
+Collection.rotate(a,k );
+for(int d:a)
+System.out.printf("%d ",d);
+}
+}
+}

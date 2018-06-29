300ac20fc4d56e6bb0724cb3eeeee0c0a992e3e2
SoundaryaJagan <38034280+SoundaryaJagan@users.noreply.github.com>
2018-06-29 12:38:50 +0000

    Update rotat.java

--- rotat.java
+++ rotat.java
@@ -8,6 +8,7 @@ public static void main(String args[])
 int n;
 Scanner sc=new Scanner(System.in);
 n=sc.nextInt();
+  int k=s.nextInt();
 ArrayList<Integer>a=new ArrayList<Integer>();
 for(int i=0;i<n;i++)
 {
